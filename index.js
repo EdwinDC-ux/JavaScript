9c8de54d44c1047c20437dfe15cc1beb67318938
EdwinDC-ux <edwin9602@gmail.com>
2020-10-07 17:16:09 -0500

    Metodos de String parte1

--- index.js
+++ index.js
@@ -21,3 +21,41 @@ console.log('nombre1 ', name1)  //Hill
 console.log('nombre2 ', name2)  //Edwin
 
 sayName()
+
+/*
+    2 -> Métodos de string Parte1.
+*/
+
+var phrase = " Hola a todos "
+
+//str.toUpperCase()
+let phraseUpper = phrase.toUpperCase()
+console.log(phraseUpper)
+
+//str.toLowerCase
+let phraseLower = phraseUpper.toLowerCase()
+console.log(phraseLower)
+
+//str.length
+let numberOfCharacters = phraseLower.length
+console.log(numberOfCharacters)
+
+//str1.concat(str2[,str3,...])
+let stringConcat = phraseLower.concat(
+    ' tengo ',
+    numberOfCharacters,
+    ' caracteres'
+)
+console.log(stringConcat)
+
+//str1.indexOf(str2)
+let findCharacter = phrase.indexOf('a')
+console.log(findCharacter)
+
+//str.search(regExpr)
+findCharacter = phrase.search(/todo/)
+console.log(findCharacter)
+
+//str1.lastIndexOf(str2)
+findCharacter = phrase.lastIndexOf('o')
+console.log(findCharacter)
